1e57a87508d97f5c9a2bd428562a12739b86a1f6
Joel Antony Joy <joejoy@deloitte.com>
2021-07-31 22:33:28 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>DocShare</h1>
+      <p>Edit documents together !</p>
+    </div>
+  );
+}
+
+export default App;
